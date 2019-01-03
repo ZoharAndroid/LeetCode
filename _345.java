605f31cc5aec35c6f4d147e2a535b4f39cb3f6d3
ZoharAndroid <1048132071@qq.com>
2019-01-03 23:12:50 +0800

    2019年1月3日

---
+++ _345.java
@@ -0,0 +1,27 @@
+/**
+ * 编写一个函数，以字符串作为输入，反转该字符串中的元音字母。
+
+示例 1:
+输入: "hello"
+输出: "holle"
+
+示例 2:
+输入: "leetcode" // eeoe lxxtcxdx
+输出: "leotcede" // 
+
+说明:
+元音字母不包含字母"y"。
+ * @author zohar
+ *
+ */
+public class _345 {
+	public static void main(String[] args) {
+		
+	}
+	
+	static class Solution {
+	    public String reverseVowels(String s) {
+	        
+	    }
+	}
+}
