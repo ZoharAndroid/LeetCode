8be9e05dade00f1c887531f0ec090ef94654ddb2
ZoharAndroid <1048132071@qq.com>
2019-01-15 13:21:49 +0800

    update 441

--- _441.java
+++ _441.java
@@ -7,6 +7,12 @@ public class _441 {
 	}
 	
 	static class Solution {
+		/**
+		 * 解题思路：第一行：1  第二行 ：1 1，第三行：1 1 1。。。。。。题目的意思就是，1为递增，1,2,3.。，直到sum<=需要判断的数，就可以直到在第几层了 
+		 * @param n
+		 * @return
+		 * 执行用时: 60 ms, 在Arranging Coins的Java提交中击败了42.75% 的用户
+		 */
 	    public int arrangeCoins(int n) {
 	    	if (n <= 0) {
 				return 0;
@@ -14,15 +20,15 @@ public class _441 {
 	    	if (n == 1) {
 				return 1;
 			}
+	    	
 	        long sum = 0;
 	        int count = 0;
 	        while(sum <= n) {
 	        	count ++;
 	        	sum += count;
 	        }
-	        
-	        
-				return count - 1;
+
+			return count - 1;
 			
 	    }
 	}
